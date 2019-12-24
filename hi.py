4b374c62cbfd697cce934eb1ef5e957a0fd74be8
sukanya-Git <59165528+sukanya-Git@users.noreply.github.com>
2019-12-24 12:48:55 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,2 +1,10 @@
-    a = "Sukanya"
-    print(a)
+import sys
+
+def Factorial(n): # return factorial
+    result = 1
+    for i in range (1,n):
+        result = result * i
+    print "factorial is ",result
+    return result
+
+print Factorial(10)
